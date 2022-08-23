6e627531039083d7176558224e4539c027c3ad48
NAOKI SHIMODA <shimoda.naoki.77s@st.kyoto-u.ac.jp>
2022-08-23 14:10:38 +0900

    040_075 fragment

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,18 @@
 import "./Child.css";
+import { Fragment } from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <Fragment key="1">
+      {/* POINT keyはFragmentが受け取ることができる唯一の属性 */}
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        フラグメントは複数のタグをreturnで返したいときに利用する。
+      </p>
+    </Fragment>
   );
 };
 
